ce19ac1afe6ec1d1f7080e7094b0b2cfa1944f4a
Ivan Petrov <ipetrov2k3@gmail.com>
2025-05-27 20:56:26 +0300

    added category screen with static table view and tap handling

--- NewsApp/Modules/Categories/CategoriesViewController.swift
+++ NewsApp/Modules/Categories/CategoriesViewController.swift
@@ -9,14 +9,62 @@ import UIKit
 
 class CategoriesViewController: UIViewController {
 
+    private let categories = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
+    private let cellIdentifier = "CategoryCell"
+    
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        
+        table.delegate = self
+        table.dataSource = self
+        
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureView()
+        setupTableView()
     }
     
     private func configureView() {
         view.backgroundColor = .systemBackground
         navigationItem.title = "Категорії"
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+extension CategoriesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.capitalized
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCategory = categories[indexPath.row]
+        print("You selected category: \(selectedCategory)")
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
