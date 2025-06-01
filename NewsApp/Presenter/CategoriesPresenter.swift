486f6d4e0d746cb541115bd73e70e6e7798ae4da
Ivan Petrov <ipetrov2k3@gmail.com>
2025-06-01 17:08:07 +0300

    introduction to MVP architecture; refactored categories screen to follow MVP architecture

---
+++ NewsApp/Presenter/CategoriesPresenter.swift
@@ -0,0 +1,89 @@
+//
+//  CategoriesPresenter.swift
+//  NewsApp
+//
+//  Created by Ivan Petrov on 01.06.2025.
+//
+
+import Foundation
+
+protocol CategoriesViewProtocol: AnyObject {
+    func showCategories(_ categories: [String])
+    func showNews(_ articles: [Article])
+    func showLoading(_ isLoading: Bool)
+    func showError(_ message: String)
+}
+
+final class CategoriesPresenter {
+    
+    // MARK: - Properties
+    
+    weak var view: CategoriesViewProtocol?
+    private let networkManager: NetworkManager
+    private let mode: CategoriesViewController.Mode
+    
+    private let categories = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
+    
+    // MARK: - Init
+    
+    init(view: CategoriesViewProtocol? = nil, networkManager: NetworkManager = .shared, mode: CategoriesViewController.Mode) {
+        self.view = view
+        self.networkManager = networkManager
+        self.mode = mode
+    }
+    
+    // MARK: - Public methods
+    
+    func viewDidLoad() {
+        switch mode {
+        case .list:
+            loadCategories()
+        case .news(let category):
+            loadNews(for: category)
+        }
+    }
+    
+    func didSelectCategory(at index: Int) -> String {
+        return categories[index]
+    }
+    
+    // MARK: - Private methods
+    
+    private func loadCategories() {
+        view?.showLoading(true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+            guard let self = self else { return }
+            self.view?.showLoading(false)
+            self.view?.showCategories(self.categories)
+        }
+    }
+    
+    /*
+    func loadCategories() {
+        view?.showLoading(true)
+        // симулируем задержку для примера
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+            self?.view?.showLoading(false)
+            self?.view?.showCategories(self?.categories ?? [])
+        }
+    }
+     */
+    
+    private func loadNews(for category: String) {
+        view?.showLoading(true)
+        
+        networkManager.request(.topHeadlines(category: category)) { [weak self] (result: Result<NewsResponse, APIError>) in
+            guard let self = self else { return }
+            
+            self.view?.showLoading(false)
+            
+            switch result {
+            case .success(let response):
+                self.view?.showNews(response.articles)
+            case .failure(let error):
+                self.view?.showError("Error loading news for category \(category): \(error.localizedDescription)")
+            }
+        }
+    }
+}
--- NewsApp/View/CategoriesViewController.swift
+++ NewsApp/View/CategoriesViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CategoriesViewController: UIViewController {
     
+    private let presenter: CategoriesPresenter
+    
     enum Mode {
         case list
         case news(category: String)
@@ -16,16 +18,27 @@ class CategoriesViewController: UIViewController {
     
     private let mode: Mode
     
+    /*
     init(mode: Mode = .list) {
         self.mode = mode
         super.init(nibName: nil, bundle: nil)
+        
+        self.presenter = CategoriesPresenter(view: self)
+    }
+     */
+    
+    init(mode: Mode = .list, presenter: CategoriesPresenter? = nil) {
+        self.mode = mode
+        self.presenter = presenter ?? CategoriesPresenter(view: nil, mode: mode)
+        super.init(nibName: nil, bundle: nil)
+        self.presenter.view = self
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private let categories = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
+    private var categories: [String] = []
     private let cellIdentifier = "CategoryCell"
     
     private var dataSource = [Article]()
@@ -48,7 +61,9 @@ class CategoriesViewController: UIViewController {
 
         configureView()
         setupTableView()
+        presenter.viewDidLoad()
         
+        /*
         switch mode {
         case .list:
             //print("Show categories list")
@@ -56,8 +71,9 @@ class CategoriesViewController: UIViewController {
         case .news(let category):
             //print("Loading news by category: \(category)")
             navigationItem.title = category.capitalized
-            obtainNews(for: category)
+            //obtainNews(for: category)
         }
+         */
     }
     
     private func configureView() {
@@ -84,6 +100,8 @@ class CategoriesViewController: UIViewController {
         ])
     }
     
+    // moved to its presenter
+    /*
     private func obtainNews(for category: String) {
         NetworkManager.shared.request(.topHeadlines(category: category)) { [weak self] (result: Result<NewsResponse, APIError>) in
             switch result {
@@ -95,6 +113,8 @@ class CategoriesViewController: UIViewController {
             }
         }
     }
+    */
+    
 }
 
 //MARK: - UITableViewDataSource & UITableViewDelegate
@@ -143,14 +163,54 @@ extension CategoriesViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch mode {
         case .list:
+            
+            /*
             let selectedCategory = categories[indexPath.row]
             //print("You selected category: \(selectedCategory)")
             let newsByCategoryVC = CategoriesViewController(mode: .news(category: selectedCategory))
             navigationController?.pushViewController(newsByCategoryVC, animated: true)
+             */
+            
+            let selectedCategory = presenter.didSelectCategory(at: indexPath.row)
+            let newsByCategoryVC = CategoriesViewController(
+                mode: .news(category: selectedCategory),
+                presenter: CategoriesPresenter(view: nil, mode: .news(category: selectedCategory))
+            )
+            
+            newsByCategoryVC.presenter.view = newsByCategoryVC
+            navigationController?.pushViewController(newsByCategoryVC, animated: true)
+            
+            
         case .news:
             print("Selected article: \(dataSource[indexPath.row])")
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+}
+
+//MARK: - CategoriesViewProtocol
+
+extension CategoriesViewController: CategoriesViewProtocol {
+    func showCategories(_ categories: [String]) {
+        self.categories = categories
+        tableView.reloadData()
+    }
+    
+    func showNews(_ articles: [Article]) {
+        self.dataSource = articles
+        tableView.reloadData()
+    }
+    
+    func showLoading(_ isLoading: Bool) {
+        // мб поставить индикатор загрузки; пока просто принт
+        print(isLoading ? "Loading..." : "Done")
+    }
+    
+    func showError(_ message: String) {
+        print("Error: ", message)
+        // мб показывать алерт
+    }
+    
+    
 }
