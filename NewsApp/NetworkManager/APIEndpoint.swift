ad2adba3436e02c54e15c94e0d25d4c630206fb6
Ivan Petrov <ipetrov2k3@gmail.com>
2025-06-06 08:40:57 +0300

    added api key

--- NewsApp/NetworkManager/APIEndpoint.swift
+++ NewsApp/NetworkManager/APIEndpoint.swift
@@ -11,13 +11,17 @@ enum APIEndpoint {
     case everything(query: String?)
     case topHeadlines(category: String)
     
+    private var apiKey: String {
+        "9dc6d99a7e52447da5285d3ea657a5e7"
+    }
+    
     var urlRequest: URLRequest {
         switch self {
         case .everything(let query):
             var components = URLComponents(string: "https://newsapi.org/v2/everything")!
             components.queryItems = [
                 URLQueryItem(name: "q", value: query ?? "apple"),
-                URLQueryItem(name: "apiKey", value: "")
+                URLQueryItem(name: "apiKey", value: apiKey)
             ]
             let url = components.url!
             return URLRequest(url: url)
@@ -27,7 +31,7 @@ enum APIEndpoint {
             components.queryItems = [
                 URLQueryItem(name: "country", value: "us"),
                 URLQueryItem(name: "category", value: category),
-                URLQueryItem(name: "apiKey", value: "")
+                URLQueryItem(name: "apiKey", value: apiKey)
             ]
             let url = components.url!
             return URLRequest(url: url)
